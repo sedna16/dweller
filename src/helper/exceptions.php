33b69e8195b84ebb4636ca15fc2fa519d94eb8ec
Jan Alvin B Dimla <alvindimla@users.noreply.github.com>
2017-01-18 17:28:05 +0800

    Update exceptions.php

--- src/helper/exceptions.php
+++ src/helper/exceptions.php
@@ -1,1 +1,32 @@
+<?php
 
+namespace dweller\helper;
+
+class exceptions {
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function throwException($msg) {
+		throw new \Exception($msg);
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function checkPHPversion() {
+		if (version_compare(PHP_VERSION, '5.3') < 0) {
+	    	self::throwException('Dweller Framework requires PHP 5.3 or above');
+	    }
+	}
+}
+
+?>

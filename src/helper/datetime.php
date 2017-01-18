1ccad308b2409e1770d3a188ba4630fd5c286477
Jan Alvin B Dimla <alvindimla@users.noreply.github.com>
2017-01-18 17:27:49 +0800

    Update datetime.php

--- src/helper/datetime.php
+++ src/helper/datetime.php
@@ -1,1 +1,66 @@
+<?php
 
+namespace dweller\helper;
+
+class datetime {
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function datetime() {
+
+		return date("Y-m-d H:i:s");
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function date() {
+		
+		return date("Y-m-d");
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function time() {
+
+		return date("H:i:s");
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function dateFormat() {
+
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function timeFormat() {
+
+	}
+}
+
+?>

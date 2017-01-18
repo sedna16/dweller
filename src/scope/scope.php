6456de82980766dd93f3502ccd64ac4fe26465a5
Jan Alvin B Dimla <alvindimla@users.noreply.github.com>
2017-01-18 17:29:18 +0800

    Update scope.php

--- src/scope/scope.php
+++ src/scope/scope.php
@@ -1,1 +1,44 @@
+<?php
 
+namespace dweller;
+
+use dweller\processors\helper as helper;
+
+class scope {
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	static public $get;
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function set($key,$value) {
+		self::$get[$key] = $value;
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function get($key) {
+		if(!helper::if_empty(self::$get[$key])) {
+			return self::$get[$key];
+			print_r(self::$get);
+		}
+	}
+}
+
+?>

efec0846a87326a3f67566e82d45aac1d8fdc6fa
Jan Alvin B Dimla <alvindimla@users.noreply.github.com>
2017-01-18 17:26:29 +0800

    Update hash.php

--- src/hashing/hash.php
+++ src/hashing/hash.php
@@ -1,1 +1,57 @@
+<?php
 
+namespace dweller;
+
+use dweller\processors\helper as helper;
+use dweller\hashing\hashprovider as hashprovider;
+use dweller\helper\exceptions as exceptions;
+
+class hash {
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	static private $cost = 12; // default cost is 10
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function password($data,$salt) {
+
+		return hashprovider::create($data,$salt);
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function re($data,$salt) {
+
+		return hashprovider::recreate($data,$salt);
+	}
+
+	/*
+	*
+	*
+	*
+	*
+	*
+	*/
+	public function verify($data,$hash) {
+
+		return hashprovider::verify($data,$hash);
+	}
+}
+
+?>
